6e9c20d57dd6655ce143fdd4dff26fb2e0e76355
Angela Wolff <warriorwomenblog@gmail.com>
2018-08-09 12:04:12 +0100

    Refactor for isMinimumTemperature

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -7,13 +7,17 @@ Thermostat.prototype.getTemperatureTarget = function(){
   return this.temperatureTarget;
 }
 
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperatureTarget === this.MINIMUMTEMPERATURE;
+}
+
 Thermostat.prototype.up = function(){
   this.temperatureTarget += 1;
 }
 
 Thermostat.prototype.down = function(){
   
-  if (this.temperatureTarget === this.MINIMUMTEMPERATURE) {
+  if (this.isMinimumTemperature()) {
     throw new Error("Cannot go below minimum temperature");
   }else {
     this.temperatureTarget -= 1;
